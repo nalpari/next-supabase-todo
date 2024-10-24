45fc96e9f992a0ec27a6f09479bcfd8b04e6834b
yoosangwook <yoo32767@gmail.com>
2024-10-24 09:56:30 +0900

    fix: add padding

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,14 +4,14 @@ import Link from 'next/link'
 export default function Home() {
   return (
     <>
-      <div className="m-2">
+      <div className="m-2 px-4">
         <h1>Home</h1>
         <p>Home page content</p>
         <p>
           <Link href="/todo">Todo page content</Link>
         </p>
 
-        <Button>Click me</Button>
+        <Button onClick={() => console.log('버튼이 클릭되었습니다!')}>Click me</Button>
       </div>
     </>
   )
